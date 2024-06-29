fc66127a3139f7fe7759f2b5f0ecf117d038f4b9
kushalchh07 <kushalchhetri54@gmail.com>
2024-06-29 22:50:00 +0545

    Feat:Sign up bloc updated

--- lib/Bloc/Signupbloc/signup_event.dart
+++ lib/Bloc/Signupbloc/signup_event.dart
@@ -1,4 +1,20 @@
 part of 'signup_bloc.dart';
 
 @immutable
-sealed class SignupEvent {}
+abstract class SignupEvent {}
+
+class SignupTappedEvent extends SignupEvent {
+  dynamic email;
+  dynamic password;
+  dynamic fname;
+  dynamic contact;
+  // dynamic lname;
+  SignupTappedEvent(
+      {required this.email,
+      required this.password,
+      required this.fname,
+      required this.contact});
+}
+
+class CheckEmailVerificationEvent extends SignupEvent {}
+class ResendEmailVerificationEvent extends SignupEvent {}
--- lib/Bloc/Signupbloc/signup_state.dart
+++ lib/Bloc/Signupbloc/signup_state.dart
@@ -4,3 +4,25 @@ part of 'signup_bloc.dart';
 sealed class SignupState {}
 
 final class SignupInitial extends SignupState {}
+final class SignupLoadingState extends SignupState {}
+final class EmailSentState extends SignupState {}
+final class SignupSuccessState extends SignupState {
+  // final dynamic success;
+  // final dynamic userId;
+  // List<LoginModel> registerResponse;
+  // SignupSuccessState({
+  //   // required this.success,
+  //   // required this.userId,
+  //   // required this.registerResponse,
+  // });
+}
+
+final class SignupErrorState extends SignupState {
+  final String error;
+
+  SignupErrorState({
+    required this.error,
+  });
+}
+
+
