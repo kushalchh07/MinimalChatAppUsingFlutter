b30c5e4e408bfbdbc26bbb012356758bfb4af5e8
kushalchh07 <kushalchhetri54@gmail.com>
2024-09-02 21:26:08 +0545

    feat: theme cubit added

---
+++ lib/theme/ThemeCubit/theme_state.dart
@@ -0,0 +1,10 @@
+part of 'theme_cubit.dart';
+
+sealed class ThemeState extends Equatable {
+  const ThemeState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class ThemeInitial extends ThemeState {}
